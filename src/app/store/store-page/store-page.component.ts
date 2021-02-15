aef8dfc8d3c04c3297e490b4d588f3d6c754beab
jaredbourget <jared.m.bourget@gmail.com>
2021-02-15 14:15:22 -0700

    Store page card layout

--- src/app/store/store-page/store-page.component.ts
+++ src/app/store/store-page/store-page.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StorePageComponent implements OnInit {
 
+  testProducts = [
+    {id: 1, imageURL: 'https://content.backcountry.com/images/items/900/PIV/PIV1WVM/HORBLU.jpg', price: 5599, name: 'Pivot Switchblade 29 Race XT Mountain Bike'},
+    {id: 2, imageURL: 'https://content.backcountry.com/images/items/900/PIV/PIV1WVM/HORBLU.jpg', price: 4599, name: 'Pivot Switchblade 29 Race XT Mountain Bike'},
+    {id: 3, imageURL: 'https://content.backcountry.com/images/items/900/PIV/PIV1WVM/HORBLU.jpg', price: 6999, name: 'Pivot Switchblade 29 Race XT Mountain Bike'},
+    {id: 4, imageURL: 'https://content.backcountry.com/images/items/900/PIV/PIV1WVM/HORBLU.jpg', price: 5999, name: 'Pivot Switchblade 29 Race XT Mountain Bike'}
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
